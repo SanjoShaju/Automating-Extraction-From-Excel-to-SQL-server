b9b7ee1fcd609614fc1fc9d0e6b2caf42dea037c
SANJO SHAJU <sanjo3024@gmail.com>
2018-08-30 10:23:44 +0530

    Update RunSQL.bat

--- RunSQL.bat
+++ RunSQL.bat
@@ -3,5 +3,5 @@ if not exist "C:\Temp\ExtractionToSQL\" mkdir C:\Temp\ExtractionToSQL
 pushd %~dp0
 cscript ConvertsExcelFileToTxtFiles.vbs
 CLS
-start sqlcmd -U "sa" -P "Intactix1" -S IN1-1023497LT -v DatabaseName = CKB_END_TO_END -i "C:\Temp\ExtractionToSQL\Extracting_To_SQL.sql"
+start sqlcmd -U "Login" -P "Password" -S Server_Name -v DatabaseName = Data1 -i "C:\Temp\ExtractionToSQL\Extracting_To_SQL.sql"
 PAUSE
